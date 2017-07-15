794ca7c0f5ecbfbeea3b0b22cf433fa24792f2ae
Evan Hubinger <evanjhub@gmail.com>
2017-07-14 23:35:46 -0700

    Fix test on Python 3

--- tests/cPyparsing_test.py
+++ tests/cPyparsing_test.py
@@ -3127,9 +3127,9 @@ class TokenMapTest(ParseTestCase):
 class ParseFileTest(ParseTestCase):
     def runTest(self):
         from cPyparsing import pyparsing_common, OneOrMore
-        s = b"""
+        s = str("""
         123 456 789
-        """
+        """)
         input_file = StringIO(s)
         integer = pyparsing_common.integer
 
